0e1b13473e4d287b2078caef10c3a6c8230a2223
lucaslemos94 <lc.lemos@outlook.com>
2021-03-04 03:07:27 -0300

    fix database credentials2.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  HOST_PORT : 'bolt://localhost:7687',
+  HOST_PORT : 'bolt://192.168.0.46:7687',
    PASSWORD : 'scisynergy',
    USER : 'neo4j'
   
