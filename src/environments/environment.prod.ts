2279e0323bbe893b2ed70c7567e23560cee3c066
lucaslemos94 <lc.lemos@outlook.com>
2021-03-08 02:21:16 -0300

    testando deploy2.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  // production: true,
-  // HOST_PORT : 'bolt://10.147.17.92:7687',
-  //  PASSWORD : 'scisynergy',
-  //  USER : 'neo4j'
+  production: true,
+  HOST_PORT : 'bolt://192.168.0.46:7687',
+   PASSWORD : 'scisynergy',
+   USER : 'neo4j'
   
 };
