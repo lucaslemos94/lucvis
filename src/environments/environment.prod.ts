a63543819c0f34a9167e42879a10c62d2ccd1046
lucaslemos94 <lc.lemos@outlook.com>
2021-03-10 02:36:29 -0300

    agora ficou legal2.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,9 @@ export const environment = {
   production: true,
   HOST_PORT : 'bolt://162.214.89.195:7687',
    PASSWORD : 'scisynergy',
-   USER : 'neo4j'
+   USER : 'neo4j',
+   year:{
+    from:1973,
+    to: 2021
+ },
 };
