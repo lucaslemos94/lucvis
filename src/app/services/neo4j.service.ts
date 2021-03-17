69c076e91fbfd02da53d9b3e6137134818f18beb
lucaslemos94 <lc.lemos@outlook.com>
2021-03-17 02:20:09 -0300

    adding encryption2.

--- src/app/services/neo4j.service.ts
+++ src/app/services/neo4j.service.ts
@@ -14,7 +14,7 @@ export class Neo4jService {
 
   constructor() { }
 
-  driver =  neo4j.driver(environment.HOST_PORT,neo4j.auth.basic(environment.USER,environment.PASSWORD),{encrypted: "ENCRYPTION_ON",trust:"TRUST_SYSTEM_CA_SIGNED_CERTIFICATES"});
+  driver =  neo4j.driver(environment.HOST_PORT,neo4j.auth.basic(environment.USER,environment.PASSWORD),{encrypted: "ENCRYPTION_ON",trust:"TRUST_CUSTOM_CA_SIGNED_CERTIFICATES"});
 
   //get a list of all universitys
   getUniversitys(universitys:University[]):void{ 
