84e328c8ce60b4f2a6fa81ebe5ef337c1a85103a
lucaslemos94 <lc.lemos@outlook.com>
2021-03-17 03:01:32 -0300

    adding http.

--- server.js
+++ server.js
@@ -10,3 +10,16 @@ res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
+
+
+if (_environment === 'production') {
+    app.use(function (req, res, next) {
+        if (req.secure) {
+            //transform to http:// request
+            res.redirect(301, 'http://' + req.headers.host + req.url);   
+            next();
+        } else {
+            next();
+        }
+    });
+}
