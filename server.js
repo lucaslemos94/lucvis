ee00ac79b7ed33fdd58c1a7723095ae56097b698
Lucas Correa <lc.lemos@outlook.com>
2021-05-18 13:26:45 -0300

    fix force http.

--- server.js
+++ server.js
@@ -9,4 +9,14 @@ app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
+app.use(function(req, res, next) {
+    if(!req.secure) {
+      return res.redirect('http://lucvis.herokuapp.com/');
+    }else if(req.secure){
+        return res.redirect('http://lucvis.herokuapp.com/')
+
+    }
+    next();
+  });
+
 app.listen(process.env.PORT || 8080);
